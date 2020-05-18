d521017932084360f916292ab0eaf49e40bf98ec
ganping <ganp1020@126.com>
2020-05-18 16:41:12 +0800

    feat：新增创建实例函数

--- axios.js
+++ axios.js
@@ -1,1 +1,18 @@
 // Created by ganping on 2020/5/18
+
+class Axios {
+  constructor(defaultConfig) {
+    this.defaultConfig = defaultConfig;
+  }
+  fetch() {
+    return Promise.resolve(1);
+  }
+}
+
+function createInstance() {
+  let context = new Axios();
+  let instance = context.fetch;
+  return instance;
+}
+
+let axios = createInstance();
